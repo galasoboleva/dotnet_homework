16d2312f69a94a997540aa793291eceb3ad8841d
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:46:56 +0500

    Task 23

---
+++ task-23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Задача 23: Показать таблицу кубов чисел от 1 до N.");
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; i <= num; i++) {
+    var result = Math.Pow(i, 3);
+    Console.Write($"{result} ");
+}
