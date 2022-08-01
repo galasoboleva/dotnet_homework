35f8e4c8690f1c624a7c9e752f4d5a86bfd95943
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:45:59 +0500

    Task 13

---
+++ task-13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Задача 13: Показать третью цифру числа");
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+if (num < 100) {
+    Console.WriteLine("третьей цифры нет");
+} else {
+    while (num > 1000) {
+        num = num/10;
+    }
+    int result = num%10;
+    Console.WriteLine(result);
+}
