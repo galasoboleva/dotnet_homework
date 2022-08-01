93fe472d79fe9fca73de0d2ca9f910b03c58c0af
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:46:23 +0500

    Task 15

---
+++ task-15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задача 15: Проверить, является ли день выходным");
+Console.WriteLine("Введите номер дня недели:");
+int day = int.Parse(Console.ReadLine());
+if (day > 5 && day <= 7) {
+    Console.WriteLine("Да");
+} else if (day > 0 && day <= 5) {
+    Console.WriteLine("Нет");
+} else {
+    Console.WriteLine("Неверное значение");
+}
