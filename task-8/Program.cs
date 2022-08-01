258bb9922ef801bfc6ae5f0acfe29b7ddcef2b24
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:45:08 +0500

    Task 8

---
+++ task-8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Задача 8: Показать все чётные числа от 1 до N.");
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine());
+for (int i = 2; i <= N; i = i + 2) {
+    Console.Write($"{i} ");
+}
