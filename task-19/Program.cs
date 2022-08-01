46c7434e437a047667c069f08cec131d62cdb15f
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:46:34 +0500

    Task 19

---
+++ task-19/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Задача 19: Определить, является ли число палиндромом");
+Console.WriteLine("Введите число:");
+int input = int.Parse(Console.ReadLine());
+
+bool isPalindrome(int num)
+{
+    int tempValue = num;
+    int reverse = 0;
+    while (tempValue > 0)
+    {
+        reverse = reverse * 10 + tempValue % 10;
+        tempValue = tempValue / 10;
+    }
+
+    return reverse == num;
+}
+
+if (isPalindrome(input)) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
