b4a788a63ef35ef5215b16f9a5758ba2302c3b1d
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:45:40 +0500

    Task 10

---
+++ task-10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Задача 10: Показать вторую цифру трехзначного числа");
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+int fraction = num/10;
+int result = fraction%10;
+Console.WriteLine(result);
