7c9e361f123612d6dd2012f9ddc60b19920105c8
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:43:48 +0500

    Task 2

---
+++ task-2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задача 2: Вывести наибольшее из двух чисел");
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) {
+    max = b;
+}
+Console.WriteLine($"Max: {max}");
