23272d1ea2a6139ee3c8ba23913918d590881eb0
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-01 11:44:12 +0500

    Task 4

---
+++ task-4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задача 4: Вывести наибольшее из трех чисел");
+Console.WriteLine("Введите первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) {
+    max = b;
+}
+if (c > max) {
+    max = c;
+}
+Console.WriteLine($"Max: {max}");
