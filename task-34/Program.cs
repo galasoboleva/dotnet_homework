2fec4fb5f91182bd51c06a1a4f89dd5e0c1a4cfe
Gala Soboleva <gala.soboleva@outlook.com>
2022-08-11 02:40:48 +0500

    Task 34

---
+++ task-34/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.WriteLine("Задача 34: посчитать количество чётных чисел в массиве");
+int[] GetArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i + 1 < array.Length)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] numbers = GetArray(4, 100, 999);
+int result = 0;
+
+foreach (int num in numbers)
+{
+    result += num % 2 == 0 ? 1 : 0;
+}
+
+PrintArray(numbers);
+Console.WriteLine(" => " + result);
